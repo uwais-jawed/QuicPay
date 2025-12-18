82955b7c5cebc110b4c0ca0c6883b29e700551f0
Uwais Jawed <142025750+uwais-jawed@users.noreply.github.com>
2025-12-18 09:50:45 +0530

    feat: refactor App component and main entry point for improved routing and structure

--- frontend/QuicPay/src/App.jsx
+++ frontend/QuicPay/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+import { Signin } from "./pages/Signin";
+import { Signup } from "./pages/Signup";
+import { Dashboard } from "./pages/Dashboard";
+import { SendMoney } from "./pages/SendMoney";
+import { UpdateProfile } from "./pages/UpdateProfile";
+import { ContactPage } from "./pages/Contact";
+import { ResetPassword } from "./pages/ResetPassword.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/signin" />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/signin" element={<Signin />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/send" element={<SendMoney />}></Route>
+          <Route path="/updateprofile" element={<UpdateProfile />}></Route>
+          <Route path="/contact" element={<ContactPage />}></Route>
+          <Route path="/resetpassword" element={<ResetPassword />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
